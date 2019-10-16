984449d41f92a7437673c2cd6bc9dec74133944e
Max Novikov <max.vashuk@gmail.com>
2019-10-17 00:07:02 +0300

    Global components & slots

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,7 +3,7 @@
     <div class="event-card -shadow">
       <span>@{{ event.time }} on {{ event.date }}</span>
       <h4>{{ event.title }}</h4>
-      <span> {{ event.attendees.length }} attending</span>
+      <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
     </div>
   </router-link>
 </template>
---
+++ src/components/MediaBox.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <slot name="heading"></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
